396ab1bc464a06d4fd1c20ce54b5d8e65a32efca
carsonology <carson.terbush@gmail.com>
2023-04-04 15:42:41 -0400

    remove districts

--- src/App.js
+++ src/App.js
@@ -7,32 +7,35 @@ import Controls from './Controls.js'
 function App() {
 
   const [hexVisibility, setHexVisibility] = useState(true)
-  const [districtVisibility, setDistrictVisibility] = useState(false)
+  // const [districtVisibility, setDistrictVisibility] = useState(false)
   const [showDeaths, setShowDeaths] = useState(true)
   const [showInjuries, setShowInjuries] = useState(true)
   const [showMinorCrashes, setShowMinorCrashes] = useState(true)
+  const [years, setYears] = useState([2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022])
 
   return (
     <div className="App">
       <Controls
         hexVisibility={hexVisibility}
         setHexVisibility={setHexVisibility}
-        districtVisibility={districtVisibility}
-        setDistrictVisibility={setDistrictVisibility}
+        // districtVisibility={districtVisibility}
+        // setDistrictVisibility={setDistrictVisibility}
         showDeaths={showDeaths}
         setShowDeaths={setShowDeaths}
         showInjuries={showInjuries}
         setShowInjuries={setShowInjuries}
         showMinorCrashes={showMinorCrashes}
         setShowMinorCrashes={setShowMinorCrashes}
-
+        years={years}
+        setYears={setYears}
       />
       <MapContext
         hexVisibility={hexVisibility}
-        districtVisibility={districtVisibility}
+        // districtVisibility={districtVisibility}
         showDeaths={showDeaths}
         showInjuries={showInjuries}
         showMinorCrashes={showMinorCrashes}
+        years={years}
       />
     </div>
   )
