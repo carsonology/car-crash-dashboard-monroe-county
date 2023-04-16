f0a476e9d5696e3906e11e7aeac5dcf9484af43a
carsonology <carson.terbush@gmail.com>
2023-04-16 15:49:05 -0400

    mobile menu interaction

--- src/MapContext.js
+++ src/MapContext.js
@@ -3,9 +3,12 @@ import './App.css'
 import Map from './Map.js'
 import mapboxgl from 'mapbox-gl'
 import fatalDataImport from './data/master_deaths.min.geojson'
+import useMobileDetect from 'use-mobile-detect-hook'
 
 function MapContext(props) {
     const {
+        menuOpen,
+        setMenuOpen,
         hexVisibility,
         speedVisibility,
         showDeaths,
@@ -15,6 +18,16 @@ function MapContext(props) {
     } = props
 
     const [fatalData, setFatalData] = useState(fatalDataImport)
+
+    // check if the user is on mobile
+    const detectMobile = useMobileDetect()
+    // if the user clicks outside of the menu on mobile, turn off the menu
+    const menuOff = () => {
+        console.log('clicking')
+        if (detectMobile.isMobile() && menuOpen) {
+            setMenuOpen(false)
+        }
+    }
 
     useEffect(() => {
         fetch(fatalData)
@@ -27,7 +40,7 @@ function MapContext(props) {
     mapboxgl.accessToken = 'pk.eyJ1IjoiY3RlcmJ1c2giLCJhIjoiY2t0dnZrYjM4MmU0aDJvbzM1dTFqbDY1NiJ9.zdZur9mZOlVhIxAoiqVwBA'
 
     return (
-        <>
+        <div onClick={menuOff}>
             <Map
                 fatalData={fatalData}
                 hexVisibility={hexVisibility}
@@ -37,7 +50,7 @@ function MapContext(props) {
                 showMinorCrashes={showMinorCrashes}
                 years={years}
             />
-        </>
+        </div>
     )
 }
 
